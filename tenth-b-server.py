4d72f6d86473af984a5a3a4f8c3745218426fbad
Matthew Walker <mattgwwalker@gmail.com>
2020-06-04 18:37:30 +1200

    First partial functional client and server

--- tenth-b-server.py
+++ tenth-b-server.py
@@ -6,6 +6,13 @@ from twisted.internet import reactor
 import pyogg
 from pyogg import opus
 import ctypes
+import queue
+import sounddevice as sd
+import sys
+import numpy
+
+q = queue.Queue(maxsize=-1)
+
 
 def createDecoder(freq, channels):
     # Just as with the encoder, to create a decoder, we must first
@@ -60,13 +67,16 @@ decoderPtr = createDecoder(decoderFreq,
 max_frame_duration = 120 # ms (See https://tools.ietf.org/html/rfc6716)
 samples_per_second = 48000
 samples_per_channel_in_buf = samples_per_second // 1000 * max_frame_duration
-samples_in_buf = samples_per_channel_in_buf * channels
-TypeBuf = ctypes.c_short * samples_in_buf
+#samples_in_buf = samples_per_channel_in_buf * channels
+TypeBuf = (ctypes.c_short*channels) * samples_per_channel_in_buf
 
 class Echo(DatagramProtocol):
 
     def datagramReceived(self, data, addr):
-        print("Received data from %s" % addr)
+        print("Received data from", addr)
+
+        print("len(data):", len(data))
+        #print(data)
 
         # Create a buffer to hold the PCM data
         buf = TypeBuf()
@@ -75,7 +85,7 @@ class Echo(DatagramProtocol):
         bufPtr = ctypes.cast(buf, ctypes.POINTER(opus.opus_int16))
 
         # Get pointer to encoded frame
-        encodedFramePtr = ctypes.cast(data, ctypes.POINTER(ctype.c_ubyte))
+        encodedFramePtr = ctypes.cast(data, ctypes.POINTER(ctypes.c_ubyte))
         numBytes = len(data)
         
         # Decode the frame
@@ -94,10 +104,65 @@ class Echo(DatagramProtocol):
             raise Exception("Decoder error detected: "+
                             opus.opus_strerror(numSamples).decode("utf"))
 
-        # TODO: Play decoded frame
+        # Put the samples on the queue to play
+        np_buf = numpy.ctypeslib.as_array(
+            buf,
+            (numSamples//channels, channels)
+        )
 
+        np_buf = np_buf[0:numSamples]
+
+        print("data placed on queue")
+        #print("channels:", channels)
+        print("shape:",np_buf.shape)
+        q.put_nowait(np_buf)
         
+
+# One second buffer
+pcm_buf = numpy.zeros((48000,2), dtype=numpy.int16)
+pcm_buf_produce_index = 0
+        
+
+def callback(outdata, frames, time, status):
+    global pcm_buf, pcm_buf_produce_index
+    try:
+        if pcm_buf_produce_index < 5000:
+            data = q.get_nowait()
+            print("Data found in queue")
+            print("type(data):", type(data))
+            print("data:", data)
+            print("len(data)", len(data))
+            print("shape:", data.shape)
+            print("pcm_buf_produce_index:",pcm_buf_produce_index)
+            pcm_buf[pcm_buf_produce_index : pcm_buf_produce_index+len(data)] = \
+                data[0:len(data)]
+            #print("pcm_buf (after data copied):",pcm_buf[pcm_buf_produce_index : pcm_buf_produce_index+len(data)])
+            pcm_buf_produce_index += len(data)
+                    
+        
+    except queue.Empty:
+        #print("Buffer is empty")
+        pass
+
+    outdata[:] = pcm_buf[:len(outdata)]
+    if pcm_buf_produce_index > 0:
+        #print("pcm_buf[:len(outdata)]:", pcm_buf[:len(outdata)])
+        #print("outdata:",outdata)
+        pcm_buf[:len(outdata)] = [[0,0]] * len(outdata)
+        pcm_buf = numpy.roll(pcm_buf, -len(outdata), axis=0)
+        pcm_buf_produce_index -= len(outdata)
+        if pcm_buf_produce_index < 0:
+            pcm_buf_produce_index = 0
+        #print("pcm_buf_produce_index:",pcm_buf_produce_index)
 
 print("Starting Server...")
 reactor.listenUDP(9999, Echo())
-reactor.run()
+
+stream = sd.OutputStream(
+    samplerate=48000,
+    callback=callback,
+    dtype=numpy.int16
+)
+
+with stream:
+    reactor.run()
